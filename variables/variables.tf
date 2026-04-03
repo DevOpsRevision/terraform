5e4159226444d2e861116d0c67585542e8beb736
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-04-03 14:53:16 -0400

    Terraform variables creation Demo

--- variables/variables.tf
+++ variables/variables.tf
@@ -2,9 +2,15 @@ variable "ami_id" {
     type = string
     default = "ami-0220d79f3f480ecf5"
     description = "AMI ID of JoinDevOps"
-  
 }
 
 variable "instance_type" {
     default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    default = {
+        Name = "Hello World"
+        Purpose = "variables-demo"
+    }
 }

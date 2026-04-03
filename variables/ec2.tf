b086871416919842fda29b709974fc49c8f06554
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-04-03 15:15:22 -0400

    Terraform variables creation Demo

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -8,26 +8,24 @@ resource "aws_instance" "roboshop" {
 
 
 resource "aws_security_group" "allow-all" {
-    name = "allow_all"
-    description = "Allow all traffic"
+    name = var.sg_name
+    description = var.sg_description
 
     ingress {
-        from_port = 0
-        to_port = 0
+        from_port = var.from_port
+        to_port = var.to_port
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
 
     egress {
-        from_port = 0
-        to_port = 0
+        from_port = var.from_port
+        to_port = var.to_port
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.cidr_blocks
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-        Name = "allow-all"
-    }
+    tags = var.sg_tags
 }
--- variables/variables.tf
+++ variables/variables.tf
@@ -14,3 +14,32 @@ variable "ec2_tags" {
         Purpose = "variables-demo"
     }
 }
+
+variable "sg_name" {
+    default = "allow-all"
+}
+
+variable "sg_description" {
+    default = "Allowing all ports from internet"
+}
+
+variable "from_port" {
+    default = 0
+}
+
+variable "to_port" {
+    type = number
+    default = 0      
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]  
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+  
+}

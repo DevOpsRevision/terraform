ade63846e576fead17eeaf7e7c004182a4999e3c
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-04-04 22:29:12 -0400

    Terraform for-loop dynamic_block R53 creation Demo

--- dynamic-block/provider.tf
+++ dynamic-block/provider.tf
@@ -1,14 +1,14 @@
-terraform{
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 #Configure the AWS provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
